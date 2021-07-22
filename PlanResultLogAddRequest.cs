42666dba8a8b84555d3da57a2a3cd20cf1567ecd
Manuel Sena <32990936+ManuelSena@users.noreply.github.com>
2021-07-22 09:47:21 -0700

    Update / Add Requests (C#)

---
+++ PlanResultLogAddRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prospect.Models.Requests.Workouts
+{
+    public class PlanResultLogAddRequest
+    {
+        
+        [Required]
+        public int PlanResultId { get; set; }
+        [Required]
+        public int DirectionTypeId { get; set; }
+        [Required]
+        public double WeighIn { get; set; }
+        [Required]
+        [MaxLength(4000, ErrorMessage = "Max Character Count 4000")]
+        public string Nutrition { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ PlanResultLogUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prospect.Models.Requests.Workouts
+{
+    public class PlanResultLogUpdateRequest : PlanResultLogAddRequest
+    {
+        [Required]
+        public string Id { get; set; }
+     
+    }
+}
